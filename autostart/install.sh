46fd311ff9bfde4285d5e7b17cb37d83efd8d157
shuck-hh <valentin.sterler@gmail.com>
2026-08-28 16:36:23 +0200

    fix: correct systemctl command from daemon-restart to daemon-reload in installation script

--- autostart/install.sh
+++ autostart/install.sh
@@ -1,6 +1,6 @@
 sudo cp cpp.service /etc/systemd/system/
 sudo cp py.service /etc/systemd/system/
-sudo systemctl daemon-restart
+sudo systemctl daemon-reload
 sudo systemctl enable cpp.service
 sudo systemctl enable py.service
 sudo systemctl start py.service
